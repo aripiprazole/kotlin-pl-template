ccb80ead6c648e9b356f721c5a6b83974f2f4cb3
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-17 13:54:18 +0000

    build: add base for language client

---
+++ editors/vscode/src/extension.ts
@@ -0,0 +1,99 @@
+/*
+ *     Copyright 2022 Gabrielle Guimarães de Oliveira
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+import { workspace, EventEmitter, ExtensionContext, window, TextDocumentChangeEvent } from 'vscode';
+
+import {
+  Disposable,
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export async function activate(_ctx: ExtensionContext) {
+  const traceOutputChannel = window.createOutputChannel('Asena Language Server Trace');
+  const command = process.env.SERVER_PATH || 'asena-language-server';
+
+  const run: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        RUST_LOG: 'error',
+        RUST_BACKTRACE: '1',
+      },
+    },
+  };
+
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: 'file', language: 'asena' }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+    },
+    traceOutputChannel,
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient('asena-language-server', 'asena language server', serverOptions, clientOptions);
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
+
+export function activateInlayHints(ctx: ExtensionContext) {
+  const maybeUpdater = {
+    hintsProvider: null as Disposable | null,
+    updateHintsEventEmitter: new EventEmitter<void>(),
+
+    async onConfigChange() {
+      this.dispose();
+    },
+
+    onDidChangeTextDocument(_event: TextDocumentChangeEvent) {
+      this.updateHintsEventEmitter.fire();
+    },
+
+    dispose() {
+      this.hintsProvider?.dispose();
+      this.hintsProvider = null;
+      this.updateHintsEventEmitter.dispose();
+    },
+  };
+
+  workspace.onDidChangeConfiguration(maybeUpdater.onConfigChange, maybeUpdater, ctx.subscriptions);
+  workspace.onDidChangeTextDocument(maybeUpdater.onDidChangeTextDocument, maybeUpdater, ctx.subscriptions);
+
+  maybeUpdater.onConfigChange().catch(console.error);
+}
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+/*
+ *     Copyright 2022 Gabrielle Guimarães de Oliveira
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+// @ts-check
+
+'use strict';
+
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  target: 'webworker', // vscode extensions run in webworker context for VS Code web 📖 -> https://webpack.js.org/configuration/target/#target
+  entry: './editors/vscode/src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+  },
+  devtool: 'hidden-source-map',
+  externals: {
+    vscode:
+        'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
+    extensions: ['.ts', '.js'],
+    alias: {},
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;

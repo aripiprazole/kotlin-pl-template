5b8706bfcb5a5571bdbff91e2bb76e16fab9ec3e
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-17 13:39:55 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
+plugins {
+  kotlin("multiplatform") version "1.8.21" apply false
+  kotlin("plugin.serialization") version "1.8.21" apply false
+  id("org.jlleitschuh.gradle.ktlint") version "10.3.0" apply false
+  id("io.gitlab.arturbosch.detekt") version "1.19.0" apply false
+}
+
+group = "dev.aripiprazole"
+version = "0.0.1"
+
+subprojects {
+  apply(plugin = "org.jetbrains.kotlin.multiplatform")
+  apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+  apply(plugin = "org.jlleitschuh.gradle.ktlint")
+  apply(plugin = "io.gitlab.arturbosch.detekt")
+
+  repositories {
+    mavenCentral()
+  }
+
+  configure<KtlintExtension> {
+    version.set("0.45.2")
+    android.set(false)
+    additionalEditorconfigFile.set(rootProject.file(".editorconfig"))
+  }
+
+  configure<DetektExtension> {
+    buildUponDefaultConfig = true
+    allRules = false
+
+    config = files("${rootProject.projectDir}/config/detekt.yml")
+    baseline = file("${rootProject.projectDir}/config/baseline.xml")
+  }
+
+  configure<KotlinMultiplatformExtension> {
+    jvmToolchain(11)
+
+    sourceSets {
+      all {
+        languageSettings {
+          optIn("kotlin.RequiresOptIn")
+          optIn("kotlin.contracts.ExperimentalContracts")
+        }
+      }
+
+      afterEvaluate {
+        get("jvmTest")?.apply {
+          kotlin.srcDirs("${project.projectDir}/jvmTest")
+        }
+
+        get("jvmMain")?.apply {
+          kotlin.srcDirs("${project.projectDir}/jvm")
+        }
+
+        get("commonMain")?.apply {
+          kotlin.srcDirs("${project.projectDir}/common")
+        }
+
+        get("commonTest")?.apply {
+          kotlin.srcDirs("${project.projectDir}/commonTest")
+        }
+      }
+    }
+  }
+}
